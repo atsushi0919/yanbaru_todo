d49866eed3b308604965fee844417b04e53ce6d9
atsushi0919 <hatake0919@yahoo.co.jp>
2021-03-29 17:15:40 +0900

    20210329

--- todo_app.rb
+++ todo_app.rb
@@ -11,33 +11,45 @@ class TodoApp
 
   # 手動入力
   def manual
-    while true
+    operation = {}
+    while operation[:method] != :quit
       operation = @manual_interface.input
-      break if operation[:method] == :quit
       execute(**operation)
     end
-    put "手動入力を終了します"
   end
 
   # 操作を実行する
   def execute(method:, params: {})
-    args = params
-    if method == :add
-      error_message = validate_add_params(**params)
-      unless error_message.empty?
-        puts error_message
-        return
-      end
+    case method
+    when :add
+      return unless valid_add_params?(**params)
       args = { task: Task.new(**params) }
+    when :quit
+      puts "手動入力を終了します"
+      return
+    else
+      args = params
     end
     @todo.public_send(method, **args)
   end
 
-  def validate_add_params(title:, content:)
+  def valid_add_params?(title:, content:)
     message = []
-    message << "【！】件名を入力してください" if title.empty?
-    message << "【！】件名は10文字以内です" if title.length > 10
-    message << "【！】内容は40文字以内です" if content.length > 40
-    message.empty? ? "" : change_message_color(message: message.join("\n"), color: "red")
+    if title.empty?
+      message << "【！】件名を入力してください"
+    end
+    if title.length > 10
+      message << "【！】件名は10文字以内です"
+    end
+    if content.length > 40
+      message << "【！】内容は40文字以内です"
+    end
+    if message.empty?
+      return true
+    else
+      #puts @todo.change_message_color(message: message.join("\n"), color: "red")
+      puts message.join("\n")
+      return false
+    end
   end
 end
